9d515c9c6763cbf344d8bf87e12710e5cb36b9fd
151250037 <151250037@smail.nju.edu.cn>
2017-03-02 19:34:35 +0800

    first test

---
+++ src/main/java/pojo/TestClass.java
@@ -0,0 +1,21 @@
+package pojo;
+
+/**
+ * Created by cuihua on 2017/3/2.
+ */
+public class TestClass {
+
+    private int value;
+
+    public TestClass(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
