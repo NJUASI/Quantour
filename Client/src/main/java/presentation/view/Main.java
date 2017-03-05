d531ab1a5e11d94672707cfb161604eb7b7cfbff
151250040 <151250040@smail.nju.edu.cn>
2017-03-05 20:36:56 +0800

    the generate jump of every panel

--- Client/src/main/java/presentation/view/Main.java
+++ Client/src/main/java/presentation/view/Main.java
@@ -1,5 +1,15 @@
 package presentation.view;
 
+
+
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 /**
  * Created by 61990 on 2017/3/5.
  */
@@ -15,5 +25,23 @@ class Main {
      */
     public static void main(String[] args) {
         new MainPanel();
+
+//        JFrame frame=new JFrame("date display");
+//
+//        JDatePickerImpl datePicker;
+//
+//        UtilDateModel model = new UtilDateModel();
+//        model.setDate(1990, 8, 24);
+//        model.setSelected(true);
+//        JDatePanelImpl datePanel = new JDatePanelImpl(model);
+//        datePicker = new JDatePickerImpl(datePanel, null);
+//        frame.setLayout(new FlowLayout());
+//        JLabel label=new JLabel("Date");
+//        JButton submit=new JButton("SUBMIT");
+//        frame.add(label);
+//        frame.add(datePicker);
+//        frame.add(submit);
+//        frame.setSize(400,400);
+//        frame.setVisible(true);
     }
 }
