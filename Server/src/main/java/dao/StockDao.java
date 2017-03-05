c77e0f48312e609bfd52e28fcf446f436ace3771
151250032 <151250032@smail.nju.edu.cn>
2017-03-05 18:26:50 +0800

    修改DAO的实现并添加注释

--- Server/src/main/java/dao/StockDao.java
+++ Server/src/main/java/dao/StockDao.java
@@ -3,6 +3,7 @@ package dao;
 import po.AveragePO;
 import po.StockPO;
 
+import java.rmi.RemoteException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,8 +13,26 @@ import java.util.List;
 public interface StockDao {
 
 
+    /**
+     * 获取特定时间段内的所有指定股票所有数据
+     *
+     * @author Byron Dong
+     * @updateTime 2017/3/5
+     * @param start 时间区域的小值
+     * @param end 时间区域的大值
+     * @param code  指定股票代码
+     * @return List<StockPO> 特定时间段内的所有指定股票所有数据
+     */
     List<StockPO> getStockData(LocalDate start, LocalDate end, String code);
 
+    /**
+     * 获取指定股票所有数据
+     *
+     * @author Byron Dong
+     * @updateTime 2017/3/5
+     * @param code  指定股票代码
+     * @return List<StockPO> 特定时间段内的所有指定股票所有数据
+     */
     List<StockPO> getStockData(String code);
 
 
