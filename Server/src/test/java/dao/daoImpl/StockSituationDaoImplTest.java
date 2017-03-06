52dd5fa2d7612fcacbf10e7fcc0b2d7df2f935dc
151250032 <151250032@smail.nju.edu.cn>
2017-03-06 18:56:56 +0800

    完成StockSituationDataHelper和Dao的Stock,StockSituation的test，由于数据尚未完全实现，后期需要返工

---
+++ Server/src/test/java/dao/daoImpl/StockSituationDaoImplTest.java
@@ -0,0 +1,39 @@
+package dao.daoImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+import po.StockSituationPO;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Byron Dong on 2017/3/6.
+ */
+public class StockSituationDaoImplTest {
+
+    private StockSituationDaoImpl stockSituationDao;
+
+    @Before
+    public void setUp() throws Exception {
+        this.stockSituationDao = new StockSituationDaoImpl();
+    }
+
+    @Test
+    public void getStockSituation() throws Exception {
+        LocalDate date = LocalDate.of(2010, 3, 4); // TODO 由于数据尚未处理，此处日期不知道结果，待定
+
+        StockSituationPO stockSituationPO = this.stockSituationDao.getStockSituation(date);
+
+        //数据都尚未确定
+        assertEquals("", stockSituationPO.getVolume());
+        assertEquals(0, stockSituationPO.getLimitUpNum());
+        assertEquals(0, stockSituationPO.getLimitDownNum());
+        assertEquals(0, stockSituationPO.getSurgingNum());
+        assertEquals(0, stockSituationPO.getSlumpingNum());
+        assertEquals(0, stockSituationPO.getClimbingNum());
+        assertEquals(0, stockSituationPO.getSlipingNum());
+    }
+
+}
---
+++ Server/src/test/java/dao/daoImpl/UserDaoImplTest.java
@@ -0,0 +1,41 @@
+package dao.daoImpl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Byron Dong on 2017/3/6.
+ */
+public class UserDaoImplTest {
+
+    //TODO 龚尘淼完成该test
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void modify() throws Exception {
+
+    }
+
+}
