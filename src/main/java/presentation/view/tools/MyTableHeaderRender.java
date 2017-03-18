32813a521c79f270106b95c3c9b3b85ab36d815d
151250032 <151250032@smail.nju.edu.cn>
2017-03-18 12:02:04 +0800

    修改Table的颜色及自定义表头

---
+++ src/main/java/presentation/view/tools/MyTableHeaderRender.java
@@ -0,0 +1,31 @@
+package presentation.view.tools;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+/**
+ * Created by Byron Dong on 2017/3/18.
+ */
+public class MyTableHeaderRender implements TableCellRenderer {
+    private static final Font labelFont = new Font("宋体",Font.PLAIN,12);
+    private TableCellRenderer delegate;
+
+    public MyTableHeaderRender(TableCellRenderer delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component c = delegate.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        if (c instanceof JLabel) {
+            JLabel label = (JLabel) c;
+            label.setBackground(new Color(41,46,51));
+            label.setForeground(new Color(201,208,214));
+            label.setFont(labelFont);
+            label.setHorizontalAlignment(SwingConstants.CENTER);
+            label.setBorder(BorderFactory.createEmptyBorder());
+        }
+        return c;
+    }
+}
