56247bbf70af0639e2fcd2f419c985adcce42248
151250040 <151250040@smail.nju.edu.cn>
2017-03-18 19:38:02 +0800

    文档个人总结

--- src/main/java/presentation/view/panel/NavigationBar.java
+++ src/main/java/presentation/view/panel/NavigationBar.java
@@ -34,7 +34,7 @@ public class NavigationBar extends TemplatePanel {
         setBackground(new Color(19,22,24));
         setBounds(adaptScreen(0,0,100,1030));
 
-        ImageIcon bgPicture= new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("picture/logo2.png"));
+        ImageIcon bgPicture= new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("picture/logo3.png"));
         JLabel logo1 =new JLabel();
         bgPicture.setImage(bgPicture.getImage().getScaledInstance(90*width/1920, 90*height/1130, Image.SCALE_DEFAULT ));
         logo1.setIcon(bgPicture);
