9110279ea6b41f61d22b661cf4cda69f09613117
151250037 <151250037@smail.nju.edu.cn>
2017-05-15 20:58:36 +0800

    Add：注册controller

--- src/main/webapp/js/logIn.js
+++ src/main/webapp/js/logIn.js
@@ -2,16 +2,19 @@
  * Created by cuihua on 2017/5/12.
  */
 function login() {
+    var userName = $("#login_username").val();
+    var password = $("#login_password").val();
+
     var jsonData = {
-        "userName": $("#username").val(),
-        "password": $("#password").val()
+        "userName": userName,
+        "password": password
     };
 
-    var obj = $.ajax({
+    $.ajax({
         type: "post",
         async: true,
         url: "/log_in",
-        contentType:'application/json;charset=UTF-8',
+        contentType: 'application/json;charset=UTF-8',
         data: JSON.stringify(jsonData),
 
         success: function (result) {
@@ -21,8 +24,8 @@ function login() {
 
             if (array[0] == "1") {
                 alert("666");
-                window.location.href = "/welcome?id=" + username;
-            } else if (array[0] == "-1"){
+                window.location.href = "/welcome?id=" + userName;
+            } else if (array[0] == "-1") {
                 // 提示错误信息
                 alert(array[1]);
             } else {
@@ -31,6 +34,42 @@ function login() {
         },
         error: function (result) {
             alert(JSON.stringify(jsonData));
+            alert("错误" + result);
+        }
+    });
+}
+
+function register() {
+    var userName = $("#reg_username").val();
+    var password1 = $("#reg_password").val();
+    var password2 = $("#reg_password2").val();
+
+    $.ajax({
+        type: "post",
+        async: true,
+        url: "/register",
+        data: {
+            "userName":userName,
+            "password": password1,
+            "password2": password2
+        },
+
+        success: function (result) {
+            alert(result);
+            var array = result.split(";");
+
+            if (array[0] == "1") {
+                alert("666");
+                window.location.href = "/welcome?id=" + userName;
+            } else if (array[0] == "-1") {
+                // 提示错误信息
+                alert(array[1]);
+            } else {
+                alert("未知错误类型orz");
+            }
+        },
+        error: function (result) {
+            alert(data);
             alert("错误" + result);
         }
     });
