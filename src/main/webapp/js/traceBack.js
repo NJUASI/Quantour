f9eeba65b6855be8fca541e8e4b147260d6507ae
151250037 <151250037@smail.nju.edu.cn>
2017-05-18 01:26:07 +0800

    Update：回测的表格分析

--- src/main/webapp/js/traceBack.js
+++ src/main/webapp/js/traceBack.js
@@ -50,6 +50,86 @@ function traceback() {
                 var certainFormates = JSON.parse(array[5]);         // List<ExcessAndWinRateDist>
                 var certainHoldings = JSON.parse(array[6]);         // List<ExcessAndWinRateDist>
 
+
+                // 回测的数值型数据
+                $("#chartPanel").toggle("slow");
+                $("#tb_chart").empty();
+                $("#tb_chart").append("<tr>");
+                for(var i = 0; i < 3; i++) {
+                    switch (i) {
+                        case 0:$("#tb_chart").append("<td>本策略</td>");
+                            break;
+                        case 1:$("#tb_chart").append("<td>基准股票</td>");
+                            break;
+                        case 2:$("#tb_chart").append("<td>相对收益</td>");
+                            break;
+                    }
+
+                    for (var j = 0; j < 7; j++) {
+                        $("#tb_chart").append("<td>" + numberValues.get(i * 7 + j) + "</td>");
+                    }
+                }
+                $("#tb_chart").append("</tr>");
+
+                // 股票周期的对比图
+                $("#cyclePanel").toggle("slow");
+                $("#tb_cycle_ab").empty();
+                $("#tb_cycle_ab").append("<tr>");
+                $("#tb_cycle_ab").append("<td>" + abReturnPeriod.positivePeriodsNum + "</td>");
+                $("#tb_cycle_ab").append("<td>" + abReturnPeriod.negativePeriodNum + "</td>");
+                $("#tb_cycle_ab").append("<td>" + abReturnPeriod.winRate + "</td>");
+                $("#tb_cycle_ab").append("</tr>");
+
+                $("#tb_cycle_re").empty();
+                $("#tb_cycle_re").append("<tr>");
+                $("#tb_cycle_re").append("<td>" + reReturnPeriod.positivePeriodsNum + "</td>");
+                $("#tb_cycle_re").append("<td>" + reReturnPeriod.negativePeriodNum + "</td>");
+                $("#tb_cycle_re").append("<td>" + reReturnPeriod.winRate + "</td>");
+                $("#tb_cycle_re").append("</tr>");
+
+
+
+                // 持有周期详情
+                $("#holdingDetailPanel").toggle("slow");
+                $("#tb_detail").empty();
+                $("#tb_detail").append("<tr>");
+                for(var i = 0; i < holdingDetails.size(); i++) {
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).periodSerial + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).startDate + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).endDate + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).holdingNum + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).strategyReturn + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).baseReturn + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).excessReturn + "</td>");
+                    $("#tb_detail").append("<td>" + holdingDetails.get(i).remainInvestment + "</td>");
+                }
+                $("#tb_detail").append("</tr>");
+
+
+                // 固定形成期的赢率分析
+                $("#certainFormatePanel").toggle("slow");
+                $("#tb_certain_formate").empty();
+                $("#tb_certain_formate").append("<tr>");
+                for(var i = 0; i < certainFormates.size(); i++) {
+                    $("#tb_certain_formate").append("<td>" + certainFormates.get(i).relativeCycle + "</td>");
+                    $("#tb_certain_formate").append("<td>" + certainFormates.get(i).excessRate + "</td>");
+                    $("#tb_certain_formate").append("<td>" + certainFormates.get(i).winRate + "</td>");
+                }
+                $("#tb_certain_formate").append("</tr>");
+
+
+                // 固定持有期的赢率分析
+                $("#certainHoldingPanel").toggle("slow");
+                $("#tb_certain_holding").empty();
+                $("#tb_certain_holding").append("<tr>");
+                for(var i = 0; i < certainHoldings.size(); i++) {
+                    $("#tb_certain_holding").append("<td>" + certainHoldings.get(i).relativeCycle + "</td>");
+                    $("#tb_certain_holding").append("<td>" + certainHoldings.get(i).excessRate + "</td>");
+                    $("#tb_certain_holding").append("<td>" + certainHoldings.get(i).winRate + "</td>");
+                }
+                $("#tb_certain_holding").append("</tr>");
+
+
                 // 处理图标的信息
                 var strategyData = JSON.parse(array[7]);            //List<List<String>>
                 var baseData = JSON.parse(array[8]);                //List<List<String>>
