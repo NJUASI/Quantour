0fb1c98f44eb73cc7b08dc031ff0b55472323c3f
151250032 <151250032@smail.nju.edu.cn>
2017-06-02 14:04:28 +0800

    实现策略托管底层

---
+++ src/main/java/com/edu/nju/asi/model/Strategy.java
@@ -0,0 +1,117 @@
+package com.edu.nju.asi.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Byron Dong on 2017/6/1.
+ */
+@Entity
+@Table(name = "strategy")
+public class Strategy {
+
+    //策略ID
+    @Id
+    @GenericGenerator(name="myGenerator",strategy = "assigned")
+    @GeneratedValue(generator = "myGenerator")
+    @Column(length = 100)
+    private String strategyID;
+
+    //策略创建日期
+    @Basic
+    private LocalDate date;
+
+    //策略创建者
+    @Basic
+    @Column(length = 100)
+    private String creater;
+
+    //策略是否私有
+    @Basic
+    @Column(columnDefinition = "bit default 0")
+    private boolean isPrivate;
+
+    //策略内容
+    @Basic
+    @Column(columnDefinition = "TEXT",nullable = true)
+    private String content;
+
+    //策略说明
+    @Basic
+    @Column(columnDefinition = "TEXT",nullable = true)
+    private String description;
+
+    @ManyToMany(mappedBy = "strategies")
+    private List<User> users = new ArrayList<>();
+
+    public Strategy() {
+    }
+
+    public Strategy(LocalDate date, String creater, boolean isPrivate, String content, String description) {
+        this.date = date;
+        this.creater = creater;
+        this.isPrivate = isPrivate;
+        this.content = content;
+        this.description = description;
+    }
+
+    public String getStrategyID() {
+        return strategyID;
+    }
+
+    public void setStrategyID(String strategyID) {
+        this.strategyID = strategyID;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCreater() {
+        return creater;
+    }
+
+    public void setCreater(String creater) {
+        this.creater = creater;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
