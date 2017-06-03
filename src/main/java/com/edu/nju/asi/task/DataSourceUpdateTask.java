046b09d640bdf5267ced52724ac0f7423bcc6536
151250037 <151250037@smail.nju.edu.cn>
2017-06-03 14:46:13 +0800

    Add：spring自动化task实现

---
+++ src/main/java/com/edu/nju/asi/task/DataSourceUpdateTask.java
@@ -0,0 +1,23 @@
+package com.edu.nju.asi.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+/**
+ * Created by cuihua on 2017/6/3.
+ *
+ * 根据数据源更新数据库
+ */
+@Component
+public class DataSourceUpdateTask {
+
+
+    // TODO 每晚23点整更新数据源
+    @Scheduled(cron = "0 0 23 * * ?")
+    public void update(){
+
+    }
+
+}
