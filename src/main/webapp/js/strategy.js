a445add694ac604836ddbb1ee6e63c178cb2c86d
151250037 <151250037@smail.nju.edu.cn>
2017-06-10 14:04:15 +0800

    Update

--- src/main/webapp/js/strategy.js
+++ src/main/webapp/js/strategy.js
@@ -1,6 +1,8 @@
 /**
  * Created by cuihua on 2017/6/8.
  */
+// 导入本js需要用的js
+document.write("<script src='traceBack.js'></script>");
 
 /**
  * 直接跳转至要查看的策略界面
@@ -91,6 +93,44 @@ $(function () {
  */
 function strategy_modify(curUser) {
 
+    // TODO 传数据
+    var strategyData = {
+        "strategyID": strategyID,
+        "date": null,
+        "creater": curUser,
+        "isPrivate": isPrivate,
+        "content": JSON.stringify(getTraceBackCriteria()),
+        "description": description,
+        "traceBackInfo": null,
+        "users": null
+    };
+
+    $.ajax({
+        type: "post",
+        async: true,
+        url: "/strategy/modify",
+        data:{
+            "strategy": JSON.stringify(strategyData)
+        },
+
+        success: function (result) {
+            // $("body").addClass("loaded");
+            var array = result.split(";");
+
+            if (array[0] == "1") {
+                alert("666\n修改成功");
+            } else if (array[0] == "-1") {
+                // 提示错误信息
+                alert(array[1]);
+            } else {
+                alert("未知错误类型orz");
+            }
+        },
+        error: function (result) {
+            alert("错误" + result);
+        }
+
+    });
 }
 
 /**
@@ -112,8 +152,7 @@ function strategy_delete() {
             var array = result.split(";");
 
             if (array[0] == "1") {
-                alert(666);
-
+                alert("666\n删除成功");
             } else if (array[0] == "-1") {
                 // 提示错误信息
                 alert(array[1]);
@@ -132,7 +171,7 @@ function strategy_delete() {
 /**
  * 非创建者订阅策略
  */
-function strategy_subscribr() {
+function strategy_subscribe() {
     var strategyID = $("#strategyName").val();
 
     $.ajax({
@@ -148,8 +187,7 @@ function strategy_subscribr() {
             var array = result.split(";");
 
             if (array[0] == "1") {
-                alert(666);
-
+                alert("666\n订阅成功");
             } else if (array[0] == "-1") {
                 // 提示错误信息
                 alert(array[1]);
@@ -167,7 +205,7 @@ function strategy_subscribr() {
 /**
  * 非创建者取消订阅策略
  */
-function strategy_revoke_subscribr() {
+function strategy_revoke_subscribe() {
     var strategyID = $("#strategyName").val();
 
     $.ajax({
@@ -183,8 +221,7 @@ function strategy_revoke_subscribr() {
             var array = result.split(";");
 
             if (array[0] == "1") {
-                alert(666);
-
+                alert("666\n取消订阅成功");
             } else if (array[0] == "-1") {
                 // 提示错误信息
                 alert(array[1]);
