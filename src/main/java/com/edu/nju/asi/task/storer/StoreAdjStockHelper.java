d9850df47e4bed8fe64b88157278508c698253e2
151250032 <151250032@smail.nju.edu.cn>
2017-06-11 22:42:41 +0800

    小修改

--- src/main/java/com/edu/nju/asi/task/storer/StoreAdjStockHelper.java
+++ src/main/java/com/edu/nju/asi/task/storer/StoreAdjStockHelper.java
@@ -21,12 +21,21 @@ import java.util.List;
  */
 public class StoreAdjStockHelper {
 
-    private String frontPath = "F:\\Quant\\adjData\\today\\front\\";
-    private String afterPath = "F:\\Quant\\adjData\\today\\after\\";
+    private String frontPath;
+    private String afterPath;
+
+    public StoreAdjStockHelper(String root) {
+
+        if(!root.endsWith(File.separator)){
+            root = root+File.separator;
+        }
+        frontPath = root+"adjData"+File.separator+"front";
+        afterPath = root+"adjData"+File.separator+"after";
+    }
 
     public void handle() {
-        storeFrontStock(frontPath+"date2");
-        storeAfterStock(afterPath+"date2");
+        storeFrontStock(frontPath);
+        storeAfterStock(afterPath);
     }
 
     private void storeFrontStock(String path) {
