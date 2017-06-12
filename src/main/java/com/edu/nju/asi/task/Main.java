e48fd74e8ba2bb513a26a4f916cb94837b6927e3
151250032 <151250032@smail.nju.edu.cn>
2017-06-12 20:35:24 +0800

    小修改，上传test

--- src/main/java/com/edu/nju/asi/task/Main.java
+++ src/main/java/com/edu/nju/asi/task/Main.java
@@ -10,18 +10,21 @@ import com.edu.nju.asi.task.fileHelper.TaskInfoHelper;
 public class Main {
 
     public static void main(String[] args) {
-        FileHelper fileHelper = new FileHelper("F:\\test\\test1");
-        fileHelper.create();
+//        FileHelper fileHelper = new FileHelper("F:\\test\\test1");
+//        fileHelper.create();
 //        System.out.println(System.getProperty("user.dir"));
+//        DataSourceUpdateTask dataSourceUpdateTask = new DataSourceUpdateTask();
+//        dataSourceUpdateTask.update();
+        Main main = new Main();
+        main.test();
     }
 
     public void test() {
         try {
-            System.out.println(getClass().getClassLoader().getResource("python/_crawing_area.py").getPath());
-//            String path = getClass().getClassLoader().getResource("python/CrawingToday.py").getPath();
-//            Process proc = Runtime.getRuntime().exec("python " + path.substring(1));
-//            proc.waitFor();
-
+            System.out.println(getClass().getClassLoader().getResource("python/CrawingToday.py").getPath());
+            String path =getClass().getClassLoader().getResource("python/CrawingToday.py").getPath();
+            Process proc = Runtime.getRuntime().exec("python "+path.substring(1));
+            proc.waitFor();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
